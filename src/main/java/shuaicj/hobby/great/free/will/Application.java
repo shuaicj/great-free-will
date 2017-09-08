8b82c05931914eef974277c5df4b93161817b9e4
shuaicj <shuaicj@gmail.com>
2017-09-08 15:48:34 +0800

    logging config

---
+++ src/main/java/shuaicj/hobby/great/free/will/Application.java
@@ -0,0 +1,18 @@
+package shuaicj.hobby.great.free.will;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot app.
+ *
+ * @author shuaicj 2017/09/08
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
