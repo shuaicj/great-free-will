4272c5255d816c91b0193915c86771e0a247a674
shuaicj <shuaicj@gmail.com>
2017-09-28 16:35:11 +0800

    add profiles

--- src/main/java/shuaicj/hobby/great/free/will/Application.java
+++ src/main/java/shuaicj/hobby/great/free/will/Application.java
@@ -1,7 +1,12 @@
 package shuaicj.hobby.great.free.will;
 
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Spring boot app.
@@ -12,7 +17,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Scope("prototype")
+    @Profile("prod")
+    public LoggingHandler loggingHandlerDefault() {
+        return new LoggingHandler(LogLevel.INFO);
+    }
+
+    @Bean
+    @Scope("prototype")
+    @Profile("!prod")
+    public LoggingHandler loggingHandlerProd() {
+        return new LoggingHandler(LogLevel.WARN);
     }
 }
